bb4b0f27e6ccbd6204c74f2d5e58c2b41c4e4e4e
kate <18845489028@163.com>
2019-05-23 15:02:25 +0800

    修改App样式

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,10 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+  list-style: none;
 }
 </style>
